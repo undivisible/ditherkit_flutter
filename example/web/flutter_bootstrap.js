99e261e945d3a2c0070b94907297abbf4a227825
Max Carter 祁明思 <undivisible@vk.com>
2026-07-30 22:07:21 -0400

    Speed up demo startup

--- example/web/flutter_bootstrap.js
+++ example/web/flutter_bootstrap.js
@@ -1,19 +1,4 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-      .getRegistrations()
-      .then((registrations) =>
-          Promise.all(registrations.map((registration) => registration.unregister())))
-      .then(() => {
-    if (navigator.serviceWorker.controller) {
-      window.location.reload();
-      return;
-    }
-    _flutter.loader.load();
-  })
-      .catch(() => _flutter.loader.load());
-} else {
-  _flutter.loader.load();
-}
+_flutter.loader.load();
